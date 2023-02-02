1a79e9f7432276f866d46cce78918603bf911a6e
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2023-02-03 01:23:44 +0530

    toss feature done

---
+++ GUI/scripts/toss.js
@@ -0,0 +1,32 @@
+let heads = 0, tails = 0;
+let tossButton = document.querySelector('#toss-btn');
+let tossCoin = document.querySelector('.toss__coin-container');
+let tossSfx = new Audio("/GUI/assets/sfx/coin-toss-sfx.mp3");
+
+tossButton.addEventListener('click', function () {
+    let i = Math.floor(Math.random() * 2)
+
+    tossCoin.style.animation = 'none'
+    tossSfx.play();
+
+    setTimeout(function () {
+        if (i) {
+            setTimeout(function () {
+                tossCoin.style.animation = 'toss-spin-heads 3s forwards';
+            }, 100);
+            heads++;
+        }
+        else {
+            setTimeout(function () {
+                tossCoin.style.animation = 'toss-spin-tails 3s forwards';
+            }, 100);
+            tails++;
+        }
+
+        tossButton.disabled = true;
+        setTimeout(function () {
+            tossButton.disabled = false;
+        }, 3000);
+
+    }, 50);
+})
