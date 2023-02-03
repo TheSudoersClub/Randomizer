8f4fdf654afcac67e2e8e96f5bd227e6304e0901
ayush <ayushahirrao@gmail.com>
2023-02-03 23:47:53 +0530

    spin wheel feature added

---
+++ GUI/scripts/edit-items-spinwheel.js
@@ -0,0 +1,65 @@
+document.getElementById('item').addEventListener("keypress", async function (e) {
+    if (e.key === "Enter") {
+        const item = document.getElementById('item').value;
+
+        if (item.length > 10) {
+            console.log("please enter the smaller string")
+        } else {
+            const target = {
+                "label": item
+            }
+
+            if (data.some(item => JSON.stringify(item) === JSON.stringify(target))) {
+                console.log('item already added');
+            } else {
+                data.push({
+                    'label': item
+                });
+                localStorage.setItem('Items', JSON.stringify(data));
+                renderItemsList();
+            }
+        }
+    }
+});
+
+document.getElementById('add-btn').addEventListener('click', () => {
+    const editItemsContainer = document.getElementById('addItem')
+    editItemsContainer.style.display = `flex`;
+
+})
+
+document.getElementById('done').addEventListener('click', async () => {
+
+    await renderSpinWheel();
+
+    const spinContainer = document.getElementById('spin-container')
+    spinContainer.style.display = `flex`;
+
+    const addItemContainer = document.getElementById('addItem')
+    addItemContainer.style.display = `none`;
+
+    const editItemsContainer = document.getElementById('editItems-container')
+    editItemsContainer.style.display = `none`;
+
+
+})
+
+function renderItemsList() {
+    const currentItems = document.getElementById('current-items');
+    currentItems.innerHTML = '';
+    data.forEach((element, index) => {
+        currentItems.innerHTML += `
+        <div class='spin-item'>
+            <div class='item-name'>${element.label}
+            <button onclick="removeItem(${index})" class="remove-btn">-</button>
+            </div>
+        </div>
+    `;
+    });
+};
+
+function removeItem(index) {
+    data.splice(index, 1);
+    localStorage.setItem('Items', JSON.stringify(data));
+    renderItemsList();
+};
--- GUI/scripts/spin-wheel.js
+++ GUI/scripts/spin-wheel.js
@@ -1,20 +1,196 @@
-let spinItems = [];
+let data = localStorage.getItem("Items");
+data = JSON.parse(data);
 
-document.getElementById('item-input').addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-        let item = document.getElementById('item-input').value;
+window.addEventListener('load', () => {
+    renderSpinWheel();
+})
 
-        if (spinItems.includes(item)) {
-            console.log('item already added');
-        } else {
-            spinItems.push(item);
-            console.log(spinItems)
-            updateItems(item);
-        }
+document.getElementById('edit-btn').addEventListener('click', () => {
+    const spinContainer = document.getElementById('spin-container')
+    spinContainer.style.display = `none`;
+
+    const editItemsContainer = document.getElementById('editItems-container')
+    editItemsContainer.style.display = `flex`;
+
+    renderItemsList();
+
+})
+
+document.getElementById('spin-btn').addEventListener('click', async () => {
+    await spin();
+})
+
+let vis, padding, svg, container, pie, arc, arcs;
+
+async function renderSpinWheel() {
+
+    const chartDiv = document.getElementById('chart');
+    chartDiv.innerHTML = ''
+
+    padding = {
+            top: 20,
+            right: 40,
+            bottom: 0,
+            left: 0
+        },
+        w = Math.min(window.innerWidth, 450) - padding.left - padding.right;
+    h = Math.min(window.innerHeight, 450) - padding.top - padding.bottom;
+    r = Math.min(w, h) / 2,
+        rotation = 0,
+        oldrotation = 0,
+        picked = 100000,
+        oldpick = [],
+        color = d3.scale.category20(); //category20c()
+
+
+    svg = await d3.select('#chart')
+        .append("svg")
+        .data([data])
+        .attr("width", w + padding.left + padding.right)
+        .attr("height", h + padding.top + padding.bottom)
+        .style({
+            // "border": "1px solid black",
+            "border-radius": "50%",
+            "padding-left": "4%",
+        });
+
+
+
+    container = await svg.append("g")
+        .attr("class", "chartholder")
+        .attr("transform", "translate(" + (w / 2 + padding.left) + "," + (h / 2 + padding.top) + ")")
+        .style({
+            // "transform": "translate(190px,241px)"
+            "box-shadow": "1px 1px 20px black",
+        })
+
+
+    vis = await container
+        .append("g");
+
+    pie = await d3.layout.pie().sort(null).value(function (d) {
+        return 1;
+    });
+
+    // declare an arc generator function
+    arc = await d3.svg.arc().outerRadius(r);
+
+    // select paths, use arc generator to draw
+    arcs = await vis.selectAll("g.slice")
+        .data(pie)
+        .enter()
+        .append("g")
+        .attr("class", "slice");
+
+    await arcs.append("path")
+        .attr("fill", function (d, i) {
+            if (data.length % 2 != 0) {
+                if (i % 3 == 0) {
+                    return "rgb(201 201 201)"
+                } else if (i % 2 == 0) {
+                    return "rgb(240 240 240)"
+                } else {
+                    return "rgb(70 70 70)"
+                }
+            } else {
+                return i % 2 == 0 ? "rgb(70 70 70)" : "rgb(220 220 220)";
+            }
+        })
+        .attr("d", function (d) {
+            return arc(d);
+        });
+
+    // add the text
+    await arcs.append("text").attr("transform", function (d) {
+            d.innerRadius = 0;
+            d.outerRadius = r;
+            d.angle = (d.startAngle + d.endAngle) / 2;
+            return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius / 2 + 20) + ")";
+        })
+        .attr("text-anchor", "middle")
+        .text(function (d, i) {
+            return data[i].label;
+        })
+        .style({
+            "font-weight": "700",
+        })
+
+    //draw spin circle
+    await container.append("circle")
+        .attr("cx", 0)
+        .attr("cy", 0)
+        .attr("r", 35)
+        .style({
+            "stroke": "rgba(0, 0, 0, 0.18)",
+            "stroke-width": "2px",
+            "fill": "white",
+            "cursor": "pointer",
+            "padding-bottom": "10px"
+
+        });
+
+    // create new circle for ::before style
+    await container.append("circle")
+        .attr("cx", 0)
+        .attr("cy", -40)
+        .attr("r", 20)
+        .style({
+            "position": " absolute",
+            "width": " 20px",
+            "height": " 30px",
+            "fill": "white",
+            "clip-path": "polygon(50% 0%, 26% 100%, 77% 100%)"
+        });
+
+
+
+
+}
+
+function rotTween(to) {
+    var i = d3.interpolate(oldrotation % 360, rotation);
+    return function (t) {
+        return "rotate(" + i(t) + ")";
+    };
+}
+
+function spin(d) {
+
+    // //all slices have been seen, all done
+    // if (oldpick.length == data.length) {
+    //     console.log("All items have been seen");
+    //     return;
+    // }
+    var ps = 360 / data.length,
+        pieslice = Math.round(1440 / data.length),
+        rng = Math.floor((Math.random() * 1440) + 360);
+
+    rotation = (Math.round(rng / ps) * ps);
+
+    picked = Math.round(data.length - (rotation % 360) / ps);
+    picked = picked >= data.length ? (picked % data.length) : picked;
+    if (oldpick.indexOf(picked) !== -1) {
+        d3.select(this).call(spin);
+        return;
+    } else {
+        oldpick.push(picked);
     }
-});
+    rotation += 90 - Math.round(ps / 2);
+    vis.transition()
+        .duration(3000)
+        .attrTween("transform", rotTween)
+        .each("end", function () {
+            //mark question as seen
+            d3.select(".slice:nth-child(" + (picked + 1) + ") path")
+            //populate question
+            d3.select("#question h1")
+                .text(data[picked].question);
+            oldrotation = rotation;
 
-function updateItems(item) {
-    const itemsDiv = document.getElementById('spin-items');
-    itemsDiv.innerHTML += `<div class='spin-item'>${item}</div>`
+            /* Get the result value from object "data" */
+            console.log(data[picked].value)
+
+            /* Comment the below line for restrict spin to sngle time */
+            container.on("click", spin);
+        });
 }
