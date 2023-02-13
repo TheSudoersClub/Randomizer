4020e33f014c30ba1aa556069df5c8b6f92dc48b
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2023-02-14 00:20:56 +0530

    add app update functionality

--- GUI/scripts/app.js
+++ GUI/scripts/app.js
@@ -1,3 +1,57 @@
+// check for updates
+if (navigator.onLine) { //check for internet connection
+    checkForUpdates().then(result => {
+        if (result === 1) { //update available
+            //display update Dialog box
+            let swp = document.querySelector('#spin-wheel-page');
+            let uc = document.querySelector('#update-container');
+            swp.style.filter = 'blur(2px)';
+            uc.style.display = 'flex';
+            swp.addEventListener('click', function (e) {
+                swp.style.filter = '';
+                uc.style.display = 'none';
+            })
+
+            //handle update-btn onclick
+            document.querySelector('#update-btn').addEventListener('click', function (e) {
+                window.open('https://github.com/TheSudoersClub/Randomizer/blob/main/GUI/build/apk/Randomizer.apk?raw=true');
+            })
+        }
+    });
+}
+
+async function checkForUpdates() {
+    let version = await getVersion("https://thesudoersclub.github.io/Randomizer/GUI/build/apk/version.txt");
+
+    if (localStorage.getItem('version') == null) {
+        localStorage.setItem("version", `${version}`);
+    }
+
+    let localVersion = localStorage.getItem('version')
+    console.log(localVersion);
+
+    if (version !== localVersion) {
+        // update available 
+        return 1;
+    } else {
+        // app is upto date
+        return 0;
+    }
+}
+
+async function getVersion(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Could not retrieve file: ${response.statusText}`);
+        }
+        const text = await response.text();
+        return text;
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+}
+
 let spinWheelPage = document.querySelector('#spin-wheel-page');
 let tossPage = document.querySelector('#toss-page');
 let initX;
